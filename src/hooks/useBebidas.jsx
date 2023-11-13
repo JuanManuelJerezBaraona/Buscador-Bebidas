ed2b155fc2dee954b5a7d3f610f8838f4bf7eb74
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2023-11-13 11:28:42 -0300

    Completed project

--- src/hooks/useBebidas.jsx
+++ src/hooks/useBebidas.jsx
@@ -1,8 +1,8 @@
 import { useContext } from "react";
 import BebidasContext from "../context/BebidasProvider";
 
-const useCategorias = () => {
-    return useContext(CategoriasContext)
+const useBebidas = () => {
+    return useContext(BebidasContext)
 }
 
-export default useCategorias
+export default useBebidas
